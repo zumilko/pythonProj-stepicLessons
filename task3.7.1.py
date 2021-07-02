e0d86f1941065b16ac9113c12fb275dfdfd9a44e
zumilko <eprim@mail.ru>
2021-07-02 12:49:42 +1100

    v.0.0.1 черновики не то

--- task3.7.1.py
+++ task3.7.1.py
@@ -23,41 +23,22 @@ Sample Output:
 Локомотив:2 2 0 0 6
 '''
 
-play, win, draw, loss, points = 0, 0, 0, 0, 0
-total = {'play': play, 'win': win, 'draw': draw, 'loss': loss, 'points': points}
-team = {}
-game, name = '', ''
-games = dict()
+team = {}; games = {}
+game = ''
 
 for j in range(int(input())):  # в range считывается первая цифра = число строк ввода
     game = input().strip().split(';') # ; print(f"{game}") # результат одной игры в списке
     j += 1
-    key = 'game_' + str(j)
-    games[key] = game
     print(game)
-    team[game[0]] = total
-    team[game[2]] = total
+    team[game[0]] = {'play': 0, 'win': 0, 'draw': 0, 'loss': 0, 'points': 0}
+    team[game[2]] = {'play': 0, 'win': 0, 'draw': 0, 'loss': 0, 'points': 0}
+#print(team)
     if int(game[1]) > int(game[3]):
         team[game[0]]['play'] += 1
         team[game[0]]['win'] += 1
         team[game[0]]['points'] += 3
         team[game[2]]['loss'] += 1
     # if int(game[1]) < int(game[3]):
-        # team[game[0]] = {'play': play + 1, 'win': win}
-        # team[game[2]] = {'play': play + 1, 'loss': loss}
+        # team[game[0]] =
+        # team[game[2]] =
 print(team)
-# print(f"\n{games}")
-# for key in games:
-#     if int(games[key][1]) > int(games[key][3]):
-#         team[games[key][0]]['play'] += 1
-#         team[games[key][0]]['win'] += 1
-#         team[games[key][0]]['points'] += 3
-
-#    if int(games[key][1]) < int(games[key][3]):
-
-#print(games)
-# team[str([x for i, x in enumerate(games[key], 1) if i == 1])] = total
-# team[str([x for i, x in enumerate(games[key], 1) if i == 3])] = total
-# print(total, team)
-
-#games = {x : {'p': j, 'w': 1} if int(game[1]) > int(game[3]) else {'p': i, 'loss': 1} for i, x in enumerate(game) if i % 2 == 0}
